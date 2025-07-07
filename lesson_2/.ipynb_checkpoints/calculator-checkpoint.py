188cecd7b199a41428057c30c6e28a77ee97cba5
Cara Colker-Eybel <ccolkereybel@gmail.com>
2025-07-07 18:28:34 -0400

    completed cacl app

---
+++ lesson_2/.ipynb_checkpoints/calculator-checkpoint.py
@@ -0,0 +1,7 @@
+# Ask user for first number
+# Ask user for second number
+# Ask user for an operation to perform
+#Perform operation on the two numbers
+#Print result to the terminal
+
+print('Welcome to Calculator')
---
+++ lesson_2/calculator.py
@@ -0,0 +1,27 @@
+# Ask user for first number
+# Ask user for second number
+# Ask user for an operation to perform
+#Perform operation on the two numbers
+#Print result to the terminal
+
+print('Welcome to Calculator')
+print("What's the first number?")
+number1 = input()
+print("What's the second number?")
+number2 = input()
+
+print(f'{number1} {number2}')
+
+print('What operation would you line to perform?\n 1) Add 2) Subtract 3) Multiply 4) Divide')
+operation = input()
+
+if operation == '1':
+    output = int(number1) + int(number2)
+elif operation == '2':
+    output = int(number1) - int(number2)
+elif operation == '3':
+    output = int(number1) * int(number2)
+elif operation == '4':
+    output = int(number1) / int(number2)
+
+print(f'The result is: {output}')
