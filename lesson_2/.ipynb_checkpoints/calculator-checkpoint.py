7294d1656f136550fe7aeff57337ab62fafbf030
Cara Colker-Eybel <ccolkereybel@gmail.com>
2025-07-07 18:30:31 -0400

    deleted extra folder

--- lesson_2/.ipynb_checkpoints/calculator-checkpoint.py
+++
@@ -1,7 +0,0 @@
-# Ask user for first number
-# Ask user for second number
-# Ask user for an operation to perform
-#Perform operation on the two numbers
-#Print result to the terminal
-
-print('Welcome to Calculator')
