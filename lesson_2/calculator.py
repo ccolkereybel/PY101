476ff310dcc668c17e65ec4d1f31ab3cf24599a0
Cara Colker-Eybel <ccolkereybel@gmail.com>
2025-07-08 10:05:26 -0400

    add operation while loop

--- lesson_2/calculator.py
+++ lesson_2/calculator.py
@@ -26,10 +26,18 @@ while invalid_number(number1):
 prompt("What's the second number?")
 number2 = input()
 
+while invalid_number(number2):
+    prompt("Hmm.. that doesn't look like a valid number.")
+    number2 = input()
+
 print(f'{number1} {number2}')
 
 print('What operation would you line to perform?\n 1) Add 2) Subtract 3) Multiply 4) Divide')
 operation = input()
+
+while operation not in ["1", "2", "3", "4"]:
+    prompt('You must choose 1, 2, 3, or 4')
+    operation = input()
 
 match operation:
     case '1':
