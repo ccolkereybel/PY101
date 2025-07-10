335b3b296e412bdc56019e38ed8b3912451ecf75
Cara Colker-Eybel <ccolkereybel@gmail.com>
2025-07-10 18:41:32 -0400

    easy1 questions complete

---
+++ lesson_3/q3.py
@@ -0,0 +1,12 @@
+famous_words = "seven years ago..."
+
+beginning_string = 'Four score and'
+
+new_string = beginning_string + famous_words
+
+print(new_string)
+
+new_string = f'Four score and {famous_words}'
+
+print(new_string)
+
